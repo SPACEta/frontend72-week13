27e8587810826f5dc48219263a9be935b1bcacf2
SPACEta <tatyana4aris@mail.ru>
2023-11-05 16:41:57 +0300

    add changed in task2

--- task2/task2.js
+++ task2/task2.js
@@ -1,28 +1,31 @@
 //Находим необходимые элементы DOM и записываем в переменные
-const birthday = document.getElementById('birthday');
+const imputBirthday = document.getElementById('imputBirthday');
 const btnBirthday = document.getElementById('btnBirthday');
 const result = document.getElementById('result');
+const errorResult = document.getElementById('errorResult');
+let currentDate = new Date();
 
-//1. Напишите функцию с именем `calculateBMI`, которая принимает два числовых параметра: вес в килограммах и рост в метрах.
-function calculateBMI() {
-    //Получаем значение, введенное в поле ввода
-    const birthdayValue = birthday.value;
+function daysBeforBirthday() {
+    let birthday = new Date(Date.parse(imputBirthday.value));
 
-    //2. Внутри функции выполните следующие шаги:
-    // -возведите рост в квадрат, используя метод `Math.pow(a, b)`.
-    const pow = Math.pow(height, 2);
+    if(birthday.value == '') {
+        errorResult.textContent = 'Введите дату своего рождения';
+    } else {
+        const timeDiff = birthday - currentDate;
+        const datePassed = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
-    // -разделите вес на квадрат роста, чтобы получить ИМТ
-    const imt = weight / pow * 10000;
+        if(datePassed === 1 || datePassed === 21 || datePassed === 31) {
+            result.textContent = "До Вашего дня рождения осталось " + datePassed + "день.";
+        } else if (datePassed === 2 || datePassed === 3 || datePassed === 4 || datePassed === 22 || datePassed === 23 || datePassed === 24) {
+            result.textContent = "До Вашего дня рождения осталось " + datePassed + "дня.";
+        } else if (datePassed >=5 && datePassed >=20) {
+            result.textContent = "До Вашего дня рождения осталось " + datePassed + "дней.";
+        } else {
+            result.textContent = "До Вашего дня рождения осталось " + datePassed + "дней.";
+        }
+    }
+}
 
-    // -округлите результат ИМТ до двух десятичных знаков с помощью метода `Math.round()`
-    const imtRound = Math.round(imt * 100.0) / 100.0;
-
-    //3. Верните значение ИМТ
-    result.innerText = "Индекс массы тела (ИМТ): " + imtRound;
-};
-
-//4. Вызовите функцию `calculateBMI` с различными значениями веса и роста и выведите результат
-btn.addEventListener('click', calculateBMI);
+btnBirthday.addEventListener('click', daysBeforBirthday);
 
 
